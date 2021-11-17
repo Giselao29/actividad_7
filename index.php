d8e048ba75e1f061c3198e8c14e5c847dc7d5e0f
gisela <giselaelisabethocampo@gmail.com>
2021-11-16 22:14:08 -0300

    Actividad 7- Gisela Ocampo

---
+++ index.php
@@ -0,0 +1,20 @@
+
+    <?php
+       echo "<br>";
+       echo "NOMBRE: ".$nombre="Gisela";
+       echo "<br>";
+       echo "<br>";
+       echo "APELLIDO:".$apellido="Ocampo";
+       echo "<br>";
+       echo "<br>";
+       echo "EDAD: ".$edad="37";
+       echo "<br>";
+       echo "<br>";
+       echo "HOBBIE: ".$hobbie="Leer, viajar";
+       echo "<br>";
+       echo "<br>";
+       echo "EDITOR DE CÓDIGO PREFERIDO: ".$editor="Visual Studio Code";
+       echo "<br>";
+       echo "<br>";
+       echo "SISTEMA OPERATIVO UTILIZADO: ".$sistema="Windows 10";
+    ?>
